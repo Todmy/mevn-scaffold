9f9b014bfaa9caca8775adda381029894479525f
Dmytro Tolok <tolok.d@gmail.com>
2018-05-17 09:02:39 +0300

    feat: Adds middlewares

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const { server } = require('config')
+const { server, cors: corsConfig } = require('config')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const app = express()
+
+app.disable('x-powered-by')
+app.enable('trust proxy')
+app.use(cors(corsConfig))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({ limit: server.jsonBodyLimit }))
 
 app.use(server.apiEndpoint, routes)
 
