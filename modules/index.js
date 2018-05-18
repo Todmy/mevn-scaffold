cb19b746f42fda13d67e1831f8d73541605c078e
Dmytro Tolok <tolok.d@gmail.com>
2018-05-18 22:02:43 +0300

    feat: Extends main module index file

--- modules/index.js
+++ modules/index.js
@@ -1,11 +1,23 @@
-const { route: authRoute } = require('./auth')
-const { route: tasksRoute } = require('./tasks')
-const { route: usersRoute } = require('./users')
+const { 
+  route: authRoute,
+} = require('./auth')
+const { 
+  route: tasksRoute,
+  Model: User,
+} = require('./tasks')
+const { 
+  route: usersRoute,
+  Model: Task,
+} = require('./users')
 
 module.exports = {
   routes: [
     authRoute,
     tasksRoute,
     usersRoute,
-  ]
+  ],
+  models: {
+    User,
+    Task,
+  }
 }
