6efa9d633bef4af3f7933a2713be4753545ec7fe
Dmytro Tolok <tolok.d@gmail.com>
2018-05-21 11:15:02 +0300

    feat: Adds TasksList component

---
+++ client/components/TaskList/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="list">
+    <div
+      v-if="!tasks.length"
+      class="list-empty"
+    >
+    <p>There is no tasks</p>
+    </div>
+
+    <ul 
+      v-else
+      class="list-filled"
+    >
+      <li 
+        v-for="(task, index) in tasks"
+        :key="index"
+      >{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskList',
+  props: {
+    tasks: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+  .list {
+    &-empty {
+      font-size: 0.8em;
+    }
+    &-filled {
+      font-size: 0.8em;
+    }
+  }
+</style>
