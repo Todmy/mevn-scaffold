629ed5503164f4e014263e8f6f7bdebcf73588b4
Dmytro Tolok <tolok.d@gmail.com>
2018-05-21 11:27:00 +0300

    feat: Adds TaskForm component

---
+++ client/components/TaskForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form 
+    class="form" 
+    @submit.prevent="onSubmit"
+  >
+    <input
+      v-model="task.name"
+      type="text" 
+      placeholder="Title"
+    >
+    <textarea 
+      v-model="task.discription"
+      type="text" 
+      placeholder="Description"
+    ></textarea>
+
+    <button 
+      type="submit"
+      class="submit-btn"  
+    >
+      Submit
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'TaskForm',
+  props: {
+    task: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  actions: {
+    onSubmit(ev) {
+      this.$emit('add', this.task)
+    }
+  },
+}
+
+</script>
+
+<style lang="scss" src="./auth.scss"></style>
