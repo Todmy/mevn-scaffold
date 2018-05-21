24b56f3d2608a73a388ac3b64b42099bb82389b8
Dmytro Tolok <tolok.d@gmail.com>
2018-05-21 22:02:50 +0300

    fix: Fixes tasks api

--- modules/tasks/controller.js
+++ modules/tasks/controller.js
@@ -1,8 +1,12 @@
+const { InternalServerError } = require('http-errors')
 const Task = require('./model')
 
 module.exports = {
   get(req, res, next) {
     Task.find(req.query, function(err, tasks) {
+      if (err) {
+        return next(new InternalServerError(err))
+      }
       res.json(tasks)
     })  
   },
@@ -11,12 +15,19 @@ module.exports = {
       owner: req.user._id
     })
     Task.create(task, function(err, task) {
+      if (err) {
+        return next(new InternalServerError(err))
+      }
       res.json(task)
     }) 
   },
   remove(req, res, next) {
-    Task.remove(req.body, function(err, task) {
-      res.json(task)
+    Task.remove(req.query, function(err, task) {
+      if (err) {
+        return next(new InternalServerError(err))
+      }
+      // TODO: refactor to return removed object
+      res.json(req.query)
     })
   },
 }
